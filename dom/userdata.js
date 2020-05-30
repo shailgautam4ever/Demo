d9ff63b40cc8965112f9899c835825ce80786a89
ROCKSTAR <rayskcoc@gmail.com>
2020-05-31 00:32:38 +0530

    added dom files

---
+++ dom/userdata.js
@@ -0,0 +1,30 @@
+let user = {
+      "gender": "male",
+      "name": {
+        "title": "Mr",
+        "first": "Joel",
+        "last": "Bowman"
+      },
+      "location": {
+        "street": {
+          "name": "Robinson Rd"
+        },
+        "city": "Tweed",
+        "state": "Queensland",
+        "country": "Australia",
+      },
+      "email": "joel.bowman@example.com",
+      "dob": {
+        "date": "1958-11-06T07:16:09.288Z",
+        "age": 62
+      },
+      "phone": "06-0374-0865",
+      "cell": "0417-933-029",
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/80.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/80.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/80.jpg"
+      },
+      "nat": "AU"
+    }
+
